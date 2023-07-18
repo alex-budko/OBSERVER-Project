fae18073f27fa011db9c29e572929d1e67b0db88
Alexander Budko <103473998+alex-budko@users.noreply.github.com>
2023-07-18 12:03:37 -0400

    a

--- backend/app2.py
+++ backend/app2.py
@@ -8,7 +8,7 @@ CORS(app)
 
 openai.api_key = "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"
 
-MODEL = "gpt-4.0"
+MODEL = "gpt-3.5-turbo"
 
 
 def process_message(original_message):
@@ -110,8 +110,7 @@ def classify_urgency(message):
 
 
 def generate_response(samples, new_message):
-    system_message = f"""Assume the role of the patient's personal physician and respond to their chat messages."""
-
+    system_message = f"""As an AI, please simulate the professional role of the patient's personal physician and interact appropriately with their chat messages. If a situation arises where an office visit is required, instruct them to visit 'my' office."""
     messages = []
 
     messages.append({"role": "system", "content": system_message})
@@ -183,36 +182,11 @@ samples = [
      "response": "Your lifestyle factors, combined with your family history, could be contributing to your symptoms and possibly increase your risk for conditions like IBS or colon cancer. It's important that you come into my office for an appointment at your earliest convenience for a thorough evaluation and necessary tests."}
 ]
 
+patient_messages = [
+    "Hey, my head hurts. What should I do?",
+]
 
-# samples = [
-#      {"message": "Just wanted to make sure that you got my reply from yesterday regarding all of my symptoms are the same - nothing different and all of the previous epidurals worked great.  Pain pretty bad so hope apt will be soon.",
-#      "response": "Good morning. I notified Dr Smith and he will call you this afternoon."},
-#     {"message": "I meant 311lbs not 211lbs geez. I also have multiple spine fractures. I am willing to try anything you have to offer to get to a healthy weight! Talk to you soon.",
-#         "response": "I am going to take do you down 50 mL x 11 hrs. This will decrease your calories and help with safe weight loss. If you have increased the rate from the original let meknow and I can adjust that for you!"},
-#     {"message": "Good morning. I wanted to confirm if Dr. Smith has reviewed my result. How does it look like?",
-#         "response": "fibroscan demonstrates changes consistent with a moderate amount of fat deposition within the liver supporting a diagnosis of fatty liver disease.  The good news is that he does not appear to have any significant fibrosis in the liver.  As a result, I think we can have him work on weight loss to see if his liver enzymes improve.  If they do, then no additional evaluation would be warranted.  If they do not improve over time, then we may consider a biopsy in the future.  He should work on weight loss as we discussed in clinic and plan to repeat his liver enzymes in 3 months. Our scheduler will contact you to arrange your follow up appointment. Please contact our office with any liver related questions as needed. Thanks"},
-#     {"message": "Have you received results from biopsy from July 19",
-#         "response": "Your biopsy was negative. Please call our office to schedule a consult to discuss your plan of care."},
-#     {"message": "I just read the x-ray report from my visit.  I’m concerned with the Cardiomegaly.  Is the enlarged heart due to the stomach being in my chest?",
-#         "response": "Thank you for your note. Abnormal position of the stomach would be unlikely to cause enlarged heart.  Pursuing with cardiologist or PCP would be appropriate."},
-#     {"message": "Good morning. I have noticed since Sunday that I has ptosis  on the right side it is not worse but it is not better. I am asymptomatic otherwise, but I get tired easy. What do you think?",
-#         "response": "Any concern for any neurologic symptoms?  I may need to see him too to make sure its not anything else going on."},
-#     {"message": "Hope you're doing well. I called yesterday about my  cold which has now turned into a sinus infection. My temp is up and I have bright orange drainage. I'm coughing. My ears are also  congested. I'm hoping you will be able to prescribe me something without an office visit since you know my history with these.",
-#         "response": "I have called in a prescription to your pharmacy. Please go to urgent care or schedule a visit if you do not start to feel better in a few days."},
-#     {"message": "Do you have my lab results?",
-#         "response": "I noticed that chemistry tests were also run at that time and the calcium is minimally high.  This should be repeated to see if that continues or was simply a lab fluctuation."},
-#     {"message": "Did you see my results? Is it ok to continue meds as prescribed?",
-#         "response": "Dr X reviewed your labs that were drawn on the 11th and says that you can continue on medication. Your labs are fine. Please take the Soriatane prior to your biggest meal each day."},
-#     {"message": "Since quitting the Sertraline an, I feel  more energy. I guess it is working. I would appreciate if you can send a refill prescription for Bupropion to my Pharmacy. Thank!",
-#         "response": "Refill sent to pharmacy. You should be able to pick up this afternoon."},
-#     {"message": "I feel like I need a boost to my oxy meds. Is there one that I can adjust to help me? I'm struggling to get anything done each day.",
-#         "response": "I really need to see you in person before making adjustments in your pain medication.  Both future appts you had scheduled with me were canceled. Please call my officeto schedule a new appointment."},
-#     {"message": "She is having a scan of her abdomen tomorrow and we need to schedule an appointment for her to see you to discuss the results and some trouble she has been having.  Please ask someone to give me a call  to set up an appointment. Thank you",
-#         "response": "That works. I'll ask for a follow up in a couple of weeks"},
-#     {"message": "I have been treated by primary physician for bronchitis that has caused wheezing, chest tightness, and a horrible cough. I had a chest x-ray Friday and it was okay. I took levaquin for 7 days.  I added musinex to the normal medications. The cough has improved a little but it is still frequent. Is there any thing else I can do to get over this sickness?",
-#         "response": "I am sorry you do not feel well. It is possible that you had viral infection"}
-# ]
+for message in patient_messages:
+    print(f"Patient Message: {message}\n")
+    response, urgency = process_message(message)
 
-original_message = "Mornin'. My leg has been feelin' weird lattely. I's scared of y'all doctors, do I still need to come in?"
-
-_, urgency = process_message(original_message)
